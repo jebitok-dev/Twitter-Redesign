7942d0bc01823eafd22060ad165c25ecea441dcf
Jebitok <sharonkosgei4@gmail.com>
2021-03-31 10:45:20 +0300

    Fix AWS Config

--- config/config.rb
+++ config/config.rb
@@ -11,12 +11,19 @@ module TwitterRedesign
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.before_initialize do
+      dev = File.join(Rails.root, 'config', 'config.yml')
+      YAML.load(File.open(dev)).each do |key,value|
+        ENV[key.to_s] = value
+      end if File.exists?(dev)
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")  
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
